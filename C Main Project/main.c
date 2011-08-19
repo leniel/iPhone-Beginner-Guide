ca6234c846ea4d384c15fe007697b7c85d7e1e29
Leniel Macaferi <leniel@Leniel-Macaferis-Mac-mini.local>
2011-08-19 19:09:21 -0300

    Initial Commit

---
+++ C Main Project/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  C Main Project
+//
+//  Created by Leniel Macaferi on 8/19/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
